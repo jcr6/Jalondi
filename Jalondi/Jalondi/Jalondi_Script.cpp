a8d0826b9f91296187cac9df812bb0aefa99cba4
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-03 20:58:17 +0200

    Includes

--- Jalondi/Jalondi/Jalondi_Script.cpp
+++ Jalondi/Jalondi/Jalondi_Script.cpp
@@ -1,9 +1,36 @@
+// Lic:
+// Jalondi
+// Script Manager
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2024
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 24.10.03
+// EndLic
 
 #include <Lunatic.hpp>
 
-#include "Jalondi_Create.hpp"
 #include <SlyvQCol.hpp>
 #include "Jalondi_Class.hpp"
+
+#include "Jalondi_Create.hpp"
+#include "Jalondi_Lua.hpp"
 
 using namespace Slyvina::Units;
 
