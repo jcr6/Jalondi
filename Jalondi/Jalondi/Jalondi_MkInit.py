1f9789a01f944b7e29a270bcfcfca14e19760827
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-03 20:57:53 +0200

    Generate C++ header from Lua script

--- Jalondi/Jalondi/Jalondi_MkInit.py
+++ Jalondi/Jalondi/Jalondi_MkInit.py
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Please note that some references to data like pictures or audio, do not automatically
 fall under this licenses. Mostly this is noted in the respective files.
 
-Version: 24.10.02
+Version: 24.10.03
 -- End License block   """
 #!/usr/bin/python
 
@@ -42,6 +42,26 @@ for file in dir:
 	Initfun+="\t%s();\n"%fun
 Initfun+="}\n\n"
 OutPut+="\n\n%s\n\n}}\n\n"%Initfun
+print("Writing: Jalondi_Init.hpp")
 with open("Jalondi_Init.hpp", "w") as text_file:
-    text_file.write(OutPut)
+	text_file.write(OutPut)
 print("Jalondi_Init.hpp -- Updated")
+print("Processing: Jalondi.lua")
+Lua=[];
+with open('Jalondi.lua') as f:
+	while True:
+		line=f.readline()
+		if not line:
+			break
+		Lua.append(line)
+print("Writing: Jalondi_Lua.hpp")        
+with open("Jalondi_Lua.hpp", "w") as f:
+	f.write("\n\n // Generated for C++ implementation: "+datetime.date.today().strftime('%Y-%m-%d')+"\n\n\n\nconst char* Lua_Jalondi {\n\t\"-- Intern --\\n\"\n");
+	for l in Lua:
+		sl = l.replace("\t","\\t");
+		sl = sl.replace("\"","\\\"");
+		sl = sl.replace("\r","")
+		sl = sl.replace("\n","")
+		f.write("\t\"%s\\n\"\n"%sl)
+	f.write("}\n\n");
+print("Ok");
