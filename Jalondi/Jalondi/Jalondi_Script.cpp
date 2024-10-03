8b76cdfe63f3c29943859c4b2faa0a39eeefe92f
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-03 22:37:45 +0200

    #7

--- Jalondi/Jalondi/Jalondi_Script.cpp
+++ Jalondi/Jalondi/Jalondi_Script.cpp
@@ -26,11 +26,14 @@
 
 #include <Lunatic.hpp>
 
+#include <SlyvBank.hpp>
 #include <SlyvQCol.hpp>
-#include "Jalondi_Class.hpp"
+#include <SlyvStream.hpp>
 
+#include "Jalondi_Class.hpp"
 #include "Jalondi_Create.hpp"
 #include "Jalondi_Lua.hpp"
+using namespace Slyvina::Lunatic;
 
 using namespace Slyvina::Units;
 
@@ -38,21 +41,84 @@ namespace Slyvina {
 	namespace Jalondi {
 
 #pragma region APIs
-
+		bool _StaakAlles{ false };
+#define CSA if (_StaakAlles) return 12;
+		static int PaniekLua(lua_State* L) {
+			auto Uitleg{ NewVecString() };
+			//cout << lua_gettop(L) << "\n";
+			Uitleg->push_back(TrSPrintF("lua_gettop(State) = %d", lua_gettop(L)));
+			for (int i = 1; i <= lua_gettop(L); i++) {
+				auto line{ TrSPrintF("Arg #%d/%d:\t",i,lua_gettop(L)) };
+				switch (lua_type(L, i)) {
+				case LUA_TSTRING:
+					line += TrSPrintF("String: \"%s\"", luaL_checkstring(L, i));
+					break;
+				case LUA_TNUMBER:
+					line += TrSPrintF("Number: %f", luaL_checknumber(L, i));
+					break;
+				case LUA_TFUNCTION:
+					line += "Function";
+					break;
+				default:
+					line += TrSPrintF("Unknown: %d", lua_type(L, i));
+					break;
+				}
+				Uitleg->push_back(line);
+			}
+			QCol->Error("Lua went in a state of panic");
+			for (auto UL : *Uitleg) QCol->Grey("\t" + UL + "\n");
+			_StaakAlles = true; 
+			return 0;
+		}
 
 		static int JA_Love(lua_State* L) {
 			// Test message and also a nice easter egg.
 			QCol->Yellow("Jalondi Smaragd loves Gyermo with all her heart\n");
 			QCol->Yellow("Read \"The Wings Of War\" to see how she will manage with those who are against it!\n");
+			QCol->Reset();
 			return 0;
+		}
+
+		static int JA_Doing(lua_State* L) {
+			QCol->Doing(luaL_checkstring(L, 1), luaL_checkstring(L,2),luaL_optstring(L,3,"\n"));
+			QCol->Reset();
+			return 0;
+		}
+
+		static int JA_FSplit(lua_State* L) {
+			auto F{ Lunatic_CheckString(L,1) };
+			Lunatic_PushString(L, ExtractDir(F));
+			Lunatic_PushString(L, StripAll(F));
+			Lunatic_PushString(L, ExtractExt(F));
+			return 3;
 		}
 		
 		static std::map<String, lua_CFunction> Jalondi_API{
-			{ "LOVE", JA_Love }
+			{ "LOVE", JA_Love },
+			{ "DOING", JA_Doing },
+			{ "FSPLIT", JA_FSplit }
 		};
 #pragma endregion
 
 #pragma region Action!
+		static String LSafe(String S) {
+			String ret{ "" };
+			for (size_t i = 0; i < S.size(); ++i) {
+				switch (S[i]) {
+				case '"': ret += "\\\"";
+				case '\t': ret += "\\t";
+				case '\n': ret += "\\n";
+				case '\r': ret += "\\r";
+				default:
+					if (S[i] >= 32 && S[i] < 127)
+						ret += S[i];
+					else
+						ret += TrSPrintF("\\x%02x", S[i]);
+				}
+			}
+			return ret;
+		}
+
 		static void Script_Explain() {
 			QCol->White("Will show all the contents of a JCR6 resource (including all external files it's been linked to)\n\n");
 			QCol->Magenta("Usage: ");
@@ -61,13 +127,42 @@ namespace Slyvina {
 			QCol->Pink(" <Lua script file> ");
 			QCol->Grey("[arguments]");
 		}
+
+		static int Script_Go(int c, char** a) {
+			_StaakAlles = false;
+			QCol->Doing("Preparing", "Lua State");
+			String
+				Script{ a[2] },
+				ArgArray{ "JALONDI_ARGS = { script=\"" + Script + "\"}\n" };
+			for (int i = 3; i < c; ++i) ArgArray += " JALONDI_ARGS[#JALONDI_ARGS+1] = \"" + LSafe(a[i]) + "\"\n";
+			Lunatic::_Lunatic::Panick = PaniekLua;
+			auto State{ Lunatic::LunaticBySource(ArgArray) }; CSA;
+			for (auto JLM : Jalondi_API) {
+				State->Register("JALONDI_" + Upper(JLM.first), JLM.second);
+			}
+			State->QDoString(Lua_Jalondi); CSA;
+			auto Ext{ Upper(ExtractExt(Script)) };
+			if (Ext == "LUA") {
+				QCol->Doing("Compiling", Script);
+				if (!FileExists(Script)) { QCol->Error("Script not found!"); return 404; }
+				State->QDoString("--[["+Script+"]]\t"+FLoadString(Script)); CSA;
+			} else if (Ext == "LUAC" || Ext == "LBC") {
+				QCol->Doing("Loading", Script);
+				if (!FileExists(Script)) { QCol->Error("Compiled script not found!"); return 404; }
+				auto Buf{ LoadCharBuf(Script) };
+				auto B{ CreateBank(Buf,FileSize(Script)) };
+				State->QDoByteCode(B); 
+				delete Buf;
+				CSA;
+			}
+		}
 #pragma endregion
 
 
 #pragma region Init
 		void Jal_Jalondi_Script() {
 			SJB("Jalondi_Script.cpp");
-			J_Action J{ "script",nullptr,Script_Explain,"Run a lua script in order to configure how to create a JCR6 file" };
+			J_Action J{ "script",Script_Go,Script_Explain,"Run a lua script in order to configure how to create a JCR6 file" };
 		}
 #pragma endregion
 	}
