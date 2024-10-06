b7ba01ab2546a419d739bb96bf57ec32251d219a
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-06 09:47:39 +0200

    vUpdate

--- Jalondi/Jalondi/Jalondi_Extract.cpp
+++ Jalondi/Jalondi/Jalondi_Extract.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 24.10.04
+// Version: 24.10.06
 // EndLic
 
 #include "Jalondi_Class.hpp"
