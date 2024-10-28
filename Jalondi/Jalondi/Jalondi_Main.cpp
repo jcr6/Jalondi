6008cc3a1db995ea346472e47be6aa86009c2532
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-28 11:54:09 +0100

    Dupe licenses removed

--- Jalondi/Jalondi/Jalondi_Main.cpp
+++ Jalondi/Jalondi/Jalondi_Main.cpp
@@ -1,28 +1,29 @@
-// Lic:
+// License:
+// 
 // Jalondi
 // Main Program
 // 
 // 
 // 
-// (c) Jeroen P. Broks, 2024
+// 	(c) Jeroen P. Broks, 2024
 // 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
 // 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // 
-// Please note that some references to data like pictures or audio, do not automatically
-// fall under this licenses. Mostly this is noted in the respective files.
+// 	Please note that some references to data like pictures or audio, do not automatically
+// 	fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 24.10.08
-// EndLic
+// Version: 24.10.28 I
+// End License
 
 // Config MUST be on top!
 #include "Jalondi_Config.hpp"
